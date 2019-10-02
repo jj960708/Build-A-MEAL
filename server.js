eab1c7a3df7fe3a8e73787c798a1118f0271e0dd
KristoferKwan <35852328+KristoferKwan@users.noreply.github.com>
2019-10-02 18:06:22 -0400

    ADD mongodb

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const router = require('express').Router();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/',(req,res) => res.send('API runing'));
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+console.log(uri);
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true }, function(error){
+    console.log(error)
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully!");
+});
 
-const PORT = process.env.PORT || 5000;
+app.get('/',(req,res) => res.send('API running'));
 
-app.listen(PORT,()=> console.log('Server runing'));
+app.listen(port, () =>{
+    //console.log(`Server is running on port: ${port}`);
+});
+
