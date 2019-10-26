bf8e4ded07019295fa61f6701c350ae708ad5965
Kristofer Kwan <kristofer.kwan@gmail.com>
2019-10-26 18:52:49 -0400

    ADD: modal popup

--- client/src/components/addInventory.component.js
+++ client/src/components/addInventory.component.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import React, { Component, useState, setShow } from 'react';
 import './stylesheets/inventory.css';
+import {Button, Modal} from 'react-bootstrap';
 
 export default class AddInventoryItem extends Component{
+    constructor(props) {
+        super(props);
+
+        this.toggle  = this.toggle.bind(this);
+        this.state = {
+          modal: false
+        }
+    }    
+
+    toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+    }
     
     render(){
         return(
-            <div className="modal" tabIndex="-1" role="dialog">
-                <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                    <h5 className="modal-title">Modal title</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div className="modal-body">
-                    <p>Modal body text goes here.</p>
-                    </div>
-                    <div className="modal-footer">
-                    <button type="button" className="btn btn-primary">Save changes</button>
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-                </div>
-            </div>
+            <>
+            <Button variant="primary" onClick={this.toggle}>
+              Launch demo modal
+            </Button>
+      
+            <Modal show={this.state.modal} onHide={this.toggle} animation={false}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.toggle}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={this.toggle}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
         )
     }
 }
